42848ac86071811d13ed316a505bd63935d5bbcb
Elise Lee <eliselee2021@u.northwestern.edu>
2019-09-06 00:35:24 -0400

    Different header

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,21 @@
         }
     });
 
+    // Animate "say hello" button scrolling to contact section
+    $('#say-hello').click(function (e) {
+
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
+
+        e.preventDefault();
+        var heading = $(this).attr('href');
+        var scrollDistance = $(heading).offset().top;
+
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, 500);
+    });
+
     // Scroll to top
     $('#to-top').click(function () {
         $('html, body').animate({
@@ -109,104 +124,104 @@
         }
     }
 
-    var scale = 1;
+    // var scale = 1;
 
-    var $parallax = $('#avatar'),
-        $eyes = $('.eyes'),
-        $upperarm = $('.upperarm'),
-        $forearm = $('.forearm'),
-        $hand = $('.hand'),
-        cx, cy, dx, dy, tiltx, tilty, radius, degree, ratioX, ratioY, transform,
-        eyeTransform = $eyes.css("transform"),
-        uArmTransform = $upperarm.css("transform"),
-        fArmTransform = $forearm.css("transform"),
-        handTransform = $hand.css("transform"),
-        newUpArm,
-        newForeArm,
-        newHand;
+    // var $parallax = $('#avatar'),
+    //     $eyes = $('.eyes'),
+    //     $upperarm = $('.upperarm'),
+    //     $forearm = $('.forearm'),
+    //     $hand = $('.hand'),
+    //     cx, cy, dx, dy, tiltx, tilty, radius, degree, ratioX, ratioY, transform,
+    //     eyeTransform = $eyes.css("transform"),
+    //     uArmTransform = $upperarm.css("transform"),
+    //     fArmTransform = $forearm.css("transform"),
+    //     handTransform = $hand.css("transform"),
+    //     newUpArm,
+    //     newForeArm,
+    //     newHand;
 
-    var mousePos = {}, hue = 0;
+    // var mousePos = {}, hue = 0;
 
-    //responsive resizing
+    // //responsive resizing
 
-    var $window = $(window);
-    var width;
-    var height;
-    var maxWidth = $parallax.width();
-    var maxHeight = $parallax.height();
+    // var $window = $(window);
+    // var width;
+    // var height;
+    // var maxWidth = $parallax.width();
+    // var maxHeight = $parallax.height();
 
-    function resize() {
-        width = $window.width();
-        height = $window.height();
+    // function resize() {
+    //     width = $window.width();
+    //     height = $window.height();
 
-        scale = Math.min(width / maxWidth, height / maxHeight) * 1.18;
+    //     scale = Math.min(width / maxWidth, height / maxHeight) * 1.18;
 
-        $parallax.css({ 'transform': 'scale(' + scale + ')' });
-    }
+    //     $parallax.css({ 'transform': 'scale(' + scale + ')' });
+    // }
 
 
-    $(window).resize(resize);
+    // $(window).resize(resize);
 
-    resize();
+    // resize();
 
     //parallaxing
 
-    $(document).mousemove(function (event) {
+    // $(document).mousemove(function (event) {
 
-        cx = Math.ceil(width / 1.8);
-        cy = Math.ceil(height / 1);
-        dx = mousePos.x - cx;
-        dy = mousePos.y - cy;
+    //     cx = Math.ceil(width / 1.8);
+    //     cy = Math.ceil(height / 1);
+    //     dx = mousePos.x - cx;
+    //     dy = mousePos.y - cy;
 
-        tiltx = (dy / cy);
-        tilty = -(dx / cx);
-        radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-        degree = (radius * 20);
-        transform = 'scale(' + scale + ') rotate3d(' + -tiltx + ', ' + -tilty + ', 0, ' + degree + 'deg)';
+    //     tiltx = (dy / cy);
+    //     tilty = -(dx / cx);
+    //     radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+    //     degree = (radius * 20);
+    //     transform = 'scale(' + scale + ') rotate3d(' + -tiltx + ', ' + -tilty + ', 0, ' + degree + 'deg)';
 
-        $parallax.css({
-            'transform': transform
-        });
+    //     $parallax.css({
+    //         'transform': transform
+    //     });
 
-        ratioX = mousePos.x / width;
-        ratioY = mousePos.y / height;
+    //     ratioX = mousePos.x / width;
+    //     ratioY = mousePos.y / height;
 
-        $eyes.css('transform', eyeTransform + " translate(" + lerp(-8, 8, ratioX) + "px, " + lerp(-3, 4, ratioY) + "px)");
+    //     $eyes.css('transform', eyeTransform + " translate(" + lerp(-8, 8, ratioX) + "px, " + lerp(-3, 4, ratioY) + "px)");
 
-        //upperarm 
-        newUpArm = uArmTransform;
-        if (ratioY < 0.5) {
-            newUpArm += " rotate(" + lerp(95, -8, ratioX) + "deg)";
-        } else {
-            newUpArm += " rotate(" + lerp(55, 20, ratioX) + "deg)";
-        }
-        newUpArm += " rotateX(" + lerp(40, -10, ratioY) + "deg)";
-        $upperarm.css('transform', newUpArm);
+    //     //upperarm 
+    //     newUpArm = uArmTransform;
+    //     if (ratioY < 0.5) {
+    //         newUpArm += " rotate(" + lerp(95, -8, ratioX) + "deg)";
+    //     } else {
+    //         newUpArm += " rotate(" + lerp(55, 20, ratioX) + "deg)";
+    //     }
+    //     newUpArm += " rotateX(" + lerp(40, -10, ratioY) + "deg)";
+    //     $upperarm.css('transform', newUpArm);
 
-        //forearm 
-        newForeArm = fArmTransform;
-        if (ratioY < 0.5) {
-            newForeArm += " rotate(" + lerp(-145, 65, ratioX) + "deg)";
-        } else {
-            newForeArm += " rotate(" + lerp(-85, 45, ratioX) + "deg)";
-        }
-        newForeArm += " rotateX(" + lerp(0, -60, ratioY) + "deg)";
-        $forearm.css('transform', newForeArm);
+    //     //forearm 
+    //     newForeArm = fArmTransform;
+    //     if (ratioY < 0.5) {
+    //         newForeArm += " rotate(" + lerp(-145, 65, ratioX) + "deg)";
+    //     } else {
+    //         newForeArm += " rotate(" + lerp(-85, 45, ratioX) + "deg)";
+    //     }
+    //     newForeArm += " rotateX(" + lerp(0, -60, ratioY) + "deg)";
+    //     $forearm.css('transform', newForeArm);
 
-        //hand 
-        newHand = handTransform;
-        if (ratioX < 0.5) {
-            newHand += " rotate(" + lerp(30, 0, ratioY) + "deg)";
-        } else {
-            newHand += " rotate(" + lerp(-10, 10, ratioY) + "deg)";
-        }
-        $hand.css('transform', newHand);
+    //     //hand 
+    //     newHand = handTransform;
+    //     if (ratioX < 0.5) {
+    //         newHand += " rotate(" + lerp(30, 0, ratioY) + "deg)";
+    //     } else {
+    //         newHand += " rotate(" + lerp(-10, 10, ratioY) + "deg)";
+    //     }
+    //     $hand.css('transform', newHand);
 
-    });
+    // });
 
-    function lerp(v0, v1, t) {
-        return v0 * (1 - t) + v1 * t;
-    }
+    // function lerp(v0, v1, t) {
+    //     return v0 * (1 - t) + v1 * t;
+    // }
 
     // color particles
 
@@ -224,6 +239,8 @@
         mousePos.y = -1;
     });
 
+    var mousePos = {}, hue = 0;
+
     var draw = setInterval(function () {
         if (mousePos.x > 0 && mousePos.y > 0) {
 
@@ -231,7 +248,7 @@
 
             var color = "background: hsla(" + hue + ",90%,60%,.6);";
             hue += 1;
-            if (hue >  360) {
+            if (hue > 360) {
                 hue = 0;
             }
 
